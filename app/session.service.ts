41705d5048f48f6cd1da623cf3bfef735c58d89a
Deciare <deciare@isisview.org>
2016-08-17 03:10:36 -0400

    SessionService: Provide Observable of login/logout events

--- app/session.service.ts
+++ app/session.service.ts
@@ -6,12 +6,32 @@
 import { Injectable } from "@angular/core";
 import { Headers, Http, Response } from "@angular/http";
 import { Cookie } from "ng2-cookies/ng2-cookies";
+import { Observable } from "rxjs/Observable";
+import { Observer } from "rxjs/Observer";
 import "rxjs/add/operator/toPromise";
 
+import { WobInfo } from "./wob";
 import { Urls } from "./urls";
+
+export class SessionEvent {
+	type: string;
+	player: WobInfo;
+
+	constructor(type: string, player?: WobInfo) {
+		this.type = type;
+		this.player = player;
+	}
+
+	// Possible values for type
+	static Login = "login";
+	static Logout = "logout";
+}
 
 @Injectable()
 export class SessionService {
+	private observer: Observer<any>;
+
+	events: Observable<any>;
 	token: string;
 
 	constructor(
@@ -20,6 +40,9 @@ export class SessionService {
 		// If authorization cookie exists, use that to initialise the token
 		var authorization = Cookie.get("authorization");
 		this.token = authorization ? authorization : undefined;
+		this.events = new Observable<any>((observer) => {
+			this.observer = observer;
+		});
 	}
 
 	handleServerError(response: Response): Promise<void> {
@@ -36,7 +59,7 @@ export class SessionService {
 		return this.token !== undefined;
 	}
 
-	getPlayerInfo(): Promise<any> {
+	getPlayerInfo(): Promise<WobInfo> {
 		var headers = new Headers({
 			"Authorization": this.token
 		});
@@ -69,6 +92,13 @@ export class SessionService {
 				if (data.success) {
 					this.token = "Bearer " + data.token;
 					Cookie.set("authorization", "Bearer " + data.token);
+
+					// Inform observers this session is logged in
+					this.getPlayerInfo()
+						.then((player: WobInfo) => {
+							this.observer.next(new SessionEvent(SessionEvent.Login, player));
+						});
+
 					return Promise.resolve("Login successful");
 				}
 				else {
@@ -81,5 +111,8 @@ export class SessionService {
 	logout() {
 		this.token = undefined;
 		Cookie.delete("authorization");
+
+		// Inform observers this session is logged out
+		this.observer.next(new SessionEvent(SessionEvent.Logout));
 	}
 }
